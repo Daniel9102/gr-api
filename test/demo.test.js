6ba65175172eedf826a6909f03de17beab05b87c
ZhangShiMing <921850425@qq.com>
2016-06-18 17:26:08 +0800

    首页商品展示多条相同记录

--- test/demo.test.js
+++ test/demo.test.js
@@ -1,9 +1,9 @@
 var should = require('chai').should();
-var AE = require("apiengine")({mode:'STAGING',scope:'common',appkey:'45883198abcdc107',masterKey:'1b7e5703602b6fce1cae7364ac0f2244'});
+var AE = require("apiengine")({mode:'STAGING',scope:'ec',appkey:'45883198abcdc107',masterKey:'1b7e5703602b6fce1cae7364ac0f2244'});
 describe('Function', function(){
     it('call function', function(done){
-        var func = new AE.Function('common.count');
-        func.invoke({table:'gr_config'}).then(function(data){
+        var func = new AE.Function('ec.server.giveMemberTimingCoupons');
+        func.invoke({}).then(function(data){
             console.log(data);
             done();
         }).catch(function(err){
